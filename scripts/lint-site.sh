f1057dadc55aa01a0262fac2beed987047f823b2
Jimmy Song <rootsongjc@gmail.com>
2019-07-26 16:39:11 +0800

    chore: add two sites into html proofer whitelist (#148)

--- scripts/lint-site.sh
+++ scripts/lint-site.sh
@@ -7,4 +7,4 @@ mdspell --version
 echo -ne "mdl "
 mdl --version
 htmlproofer --version
-htmlproofer ./public --assume-extension --check-html --check-external-hash --check-opengraph --alt-ignore '/.*/' --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/groups.google.com/forum/,/metrics20.org/,/static.javadoc.io/,/dashboard.dev.sofastack.tech/,/127.0.0.1/,/zipkin-dev.sofastack.tech/,/codecov.io/"
+htmlproofer ./public --assume-extension --check-html --check-external-hash --check-opengraph --alt-ignore '/.*/' --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/groups.google.com/forum/,/metrics20.org/,/static.javadoc.io/,/dashboard.dev.sofastack.tech/,/127.0.0.1/,/zipkin-dev.sofastack.tech/,/codecov.io/,/tech.antfin.com/,/zhuanlan.zhihu.com/"
