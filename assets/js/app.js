7f0e66c0d2ec294b916a3d0503f81c91f0e700c1
Hydrogen <fanyangliang@gmail.com>
2019-08-12 11:26:28 +0800

    feat: lightbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,10 @@ import menuFunc from './modules/menu'
 import tocFunc from './modules/toc'
 import asideFunc from './modules/aside'
 import searchFunc from './modules/search'
+import zoom from 'zoom-image'
+import 'zoom-image/css/zoom-image.css'
+
+import { $$ } from './modules/utils'
 
 const main = () => {
   // Menu
@@ -15,6 +19,11 @@ const main = () => {
 
   // search page
   searchFunc()
+
+  // image zoon
+  $$('.typo img').forEach(imgElem => {
+    zoom(imgElem)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
