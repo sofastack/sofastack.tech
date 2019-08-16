4891edb826a10825f77283e62b78748744555667
Hydrogen <fanyangliang@gmail.com>
2019-08-16 17:50:40 +0800

    feat: sprint-1.3-part-2 (#200)
    * feat: add search page
    
    - refactor asset
    - add algoliasearch
    
    * feat: add menu search input
    
    * feat: menu TOC
    
    * feat: search with type
    
    * fix: menu TOC close
    
    * feat: guide aside toc
    
    * feat: lightbox
    
    * feat: new logo
    
    * feat: new cover for list page
    
    * fix: remove baseURL #163
    
    Just set baseURL""
    
    #163
    
    * fix: pagination overflow
    
    * fix: card background style
    
    * fix: pagination crash

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,12 @@ import menuFunc from './modules/menu'
 import tocFunc from './modules/toc'
 import asideFunc from './modules/aside'
 import searchFunc from './modules/search'
+import paginationFunc from './modules/pagination'
+
+import zoom from 'zoom-image'
+import 'zoom-image/css/zoom-image.css'
+
+import { $$ } from './modules/utils'
 
 const main = () => {
   // Menu
@@ -15,6 +21,14 @@ const main = () => {
 
   // search page
   searchFunc()
+
+  // pagination page
+  paginationFunc()
+
+  // image zoon
+  $$('.typo img').forEach(imgElem => {
+    zoom(imgElem)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
