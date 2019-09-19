a0eaf1343a424dd599763ec489236aa91598fb7c
Hydrogen <fanyangliang@gmail.com>
2019-09-19 12:21:25 +0800

    feat: feat: collapse tags

---
+++ assets/js/i18n.js
@@ -0,0 +1,9 @@
+export const en = {
+  expand: 'Expand',
+  collapse: 'Collapse',
+}
+
+export const zh = {
+  expand: '全部展开',
+  collapse: '收起',
+}
--- assets/js/modules/utils.js
+++ assets/js/modules/utils.js
@@ -1,4 +1,15 @@
+import get from 'lodash-es/get'
+import { en, zh } from '../i18n'
 
+/**
+ * i18n
+ */
+export function i18n(path) {
+  const lang = window.SITE_LANGUAGE
+  const langJOSN = lang === "zh" ? zh : en
+
+  return get(langJOSN, path, "")
+}
 
 /**
  * DOM Selector methods
